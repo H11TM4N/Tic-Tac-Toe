064cb673856b2a7327d562c0f57ecb0227e1069f
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-18 15:29:44 +0100

    UI update

--- lib/widgets/elevated_button.dart
+++ lib/widgets/elevated_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/Screens/ai_screen.dart';
+import 'package:tic_tac_toe/Screens/friend_screen.dart';
 
 class MyElevatedButton extends StatelessWidget {
   final String text;
@@ -12,9 +14,22 @@ class MyElevatedButton extends StatelessWidget {
       padding: const EdgeInsets.all(10.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          fixedSize: Size(mediaQueryData.size.width * .55, 40),
+          fixedSize: Size(mediaQueryData.size.width * .65, 40),
         ),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) {
+                if (text == 'With AI') {
+                  return const AIScreen();
+                } else {
+                  return const FriendScreen();
+                }
+              },
+            ),
+          );
+        },
         child: Text(text),
       ),
     );
