f762aec0f631da68b0936f104d3c96cb5aecdd49
JER3MIAH <jeremiahbrownie@gmail.com>
2024-11-23 13:23:15 +0100

    Remove server files

--- server/server.js
+++
@@ -1,7 +0,0 @@
-import config from "./src/config/config.js";
-import { server } from "./app.js";
-import './src/socket.js'; 
-
-server.listen(config.PORT, () => {
-    console.log(`Listening on port ${config.PORT}`);
-});
